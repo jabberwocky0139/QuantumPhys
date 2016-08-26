8d4a6308e2a098ac4ddfa36fa59674f0307d0595
“jabberwocky0139” <torii0139@asagi.waseda.jp>
2016-08-26 18:02:49 +0900

    1ファイルにまとめた

---
+++ Modules/auto/12-Zero_BdG.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "12-Zero_BdG"
+ (lambda ()
+   (LaTeX-add-labels
+    "picture1"
+    "picture2"
+    "unitary1"
+    "unitary2"
+    "partition"
+    "Perturbed-thermo1"
+    "Perturbed-thermo2"
+    "Zeromode-expansion"
+    "plane"
+    "ZeromodeHamiltonian"
+    "Perturbed-thermo3"
+    "Hamiltonian-Exp"
+    "Zeromode-Exp"
+    "BdG-Exp"
+    "interactionexpectation"
+    "Interaction-Exp"
+    "self-consistent"
+    "interaction-expectation"))
+ :latex)
+
